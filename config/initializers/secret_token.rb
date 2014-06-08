961f776407530b88d6a21f7ea207f6f8292bdf65
Leslie T <leslie_tanengsy@yahoo.com>
2014-06-08 12:42:44 +1000

    add Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtsi::Application.config.secret_token = '9a8594e7a9e3e21ecfce83adf07f025b7f957190ea4aa0ec67dfbcdcdabf4d629a54c44ab21e65774103ce39ac389af044136c2bf0e82074d55576dbc15a108d'
